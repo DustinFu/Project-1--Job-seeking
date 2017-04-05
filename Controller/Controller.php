c6e41ac8d4020f70b34d1de877731e9eca0dbbc0
rmit-s3482460-Yuze-Li <s3482460@student.rmit.edu.au>
2017-04-05 14:58:41 +1000

    controller

---
+++ Controller/Controller.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+use View;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use Illuminate\Http\Request;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    public function __construct() {
+
+       $base_url = "/jobseek/jobseeking/public";
+       View::share ( 'base_url', $base_url );
+    } 
+
+    // Get user info from token provided in HTTP POST parameter "post_token"
+    public function getUser(Request $request){
+        
+        try {
+            $token = $request->input('post_token'); // HTTP POST BODY
+            if(!empty($token)){
+                // Token in POST BODY PARAM (token = XXX)
+                if (! $user = JWTAuth::setToken($token)->authenticate()) {
+                    //return response()->json(['user_not_found'], 404);
+                    return null;
+                }
+            }else{
+                return null; // No token in HTTP POST parameter
+
+                // This throws exception somehow cannot catch??
+                // Token in HEADER or HTTP GET PARAM (http://...?token=XXX)
+                //if (! $user = JWTAuth::parseToken()->authenticate()) {
+                    //return response()->json(['user_not_found'], 404);
+                //    return null;
+                //}
+            }
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+ 
+            //return response()->json(['token_expired'], $e->getStatusCode());
+            return null;
+ 
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+ 
+            //return response()->json(['token_invalid'], $e->getStatusCode());
+            return null;
+
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+ 
+            //return response()->json(['token_absent'], $e->getStatusCode());
+            return null;
+        }
+ 
+        return $user;
+    }
+
+}
---
+++ Controller/JobController.php
@@ -0,0 +1,168 @@
+<?php
+namespace App\Http\Controllers;
+
+use Log;
+use Validator;
+use DB;
+
+use App\Classification; // Model
+use App\Type;  // Model
+use App\Location; // Model
+use App\Job; // Model
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class JobController extends Controller
+{
+    public $viewDir = "job";
+
+    public function __construct(){
+       parent::__construct();
+    }
+    
+    public function index()
+    {
+        $records = Job::findRequested();
+        return $this->view( "index", ['records' => $records] );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return  \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return $this->view("create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param    \Illuminate\Http\Request  $request
+     * @return  \Illuminate\Http\Response
+     */
+    public function store( Request $request )
+    {
+        $this->validate($request, Job::validationRules());
+
+        Job::create($request->all());
+
+        return redirect('/job');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return  \Illuminate\Http\Response
+     */
+    public function show(Request $request, Job $job)
+    {
+        return $this->view("show",['job' => $job]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @return  \Illuminate\Http\Response
+     */
+    public function edit(Request $request, Job $job)
+    {
+        return $this->view( "edit", ['job' => $job] );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param    \Illuminate\Http\Request  $request
+     * @return  \Illuminate\Http\Response
+     */
+    public function update(Request $request, Job $job)
+    {
+        if( $request->isXmlHttpRequest() )
+        {
+            $data = [$request->name  => $request->value];
+            $validator = \Validator::make( $data, Job::validationRules( $request->name ) );
+            if($validator->fails())
+                return response($validator->errors()->first( $request->name),403);
+            $job->update($data);
+            return "Record updated";
+        }
+
+        $this->validate($request, Job::validationRules());
+
+        $job->update($request->all());
+
+        return redirect('/job');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return  \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Job $job)
+    {
+        $job->delete();
+        return redirect('/job');
+    }
+
+    protected function view($view, $data = [])
+    {
+        return view($this->viewDir.".".$view, $data);
+    }
+
+    // post job page
+    public function postjob_page( Request $request )
+    {
+        $types = Type::all();
+        $locations = Location::all();
+        $classifications = Classification::all();
+
+        return view('postjob', ['types' => $types, 
+                                'locations' => $locations,
+                                'classifications' => $classifications
+                               ]); 
+    }
+
+    // post job page call this API
+    public function postjob_api( Request $request )
+    {
+
+/*   [validation] Option 1 to handle input error : 
+        $validator = Validator::make($request->all(), Job::validationRules());
+        if ($validator->fails()) {
+            $message = $validator->errors();
+            Log::info('postjob_api Validator error : '.$message);
+            return response()->json($message, 500);
+        }
+*/
+
+
+//   [validation] Option 2 to handle input error : this method will redirect back to the input view page with $errors
+        $validate_return = $this->validate($request, Job::validationRules());
+        $create_return = Job::create($request->all());
+        
+        Log::info('postjob_api create_return: '.$create_return);
+
+        return redirect('/showjob/'.$create_return->id);
+    }
+
+    public function showjob_page(Request $request, Job $job)
+    {
+        // Raw SQL : 
+        //$query_result = DB::select('SELECT * FROM jobs WHERE id = '.$job->id);
+        //Log::info('showjob_page query_result: ',  $query_result);
+
+
+        return view("showjob",['job' => $job]);
+    }
+
+    public function findjob_page(Request $request)
+    {
+        return view("findjob");
+    }
+
+}
